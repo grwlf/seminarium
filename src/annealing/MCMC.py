1bc7ff432022c485783a6529014b1dc54e684cbf
Sergey Mironov <grrwlf@gmail.com>
2021-11-02 01:02:40 +0300

    Update src/annealing

--- src/annealing/MCMC.py
+++ src/annealing/MCMC.py
@@ -9,7 +9,8 @@ from pylightnix import (Registry, Config, Build, DRef, RRef, realize1,
 import numpy as np
 from numpy.random import seed as np_seed, choice as np_choice
 from math import sin, exp
-from typing import (Any, Optional, Union, Callable, List, Tuple)
+from typing import (Any, Optional, Union, Callable, List, Tuple, TypeVar,
+                    Generic)
 from functools import partial
 from json import dump as json_dump, load as json_load, loads as json_loads
 from collections import defaultdict
@@ -19,6 +20,7 @@ from itertools import chain
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot, hist, subplots
 from dataclasses_json import dataclass_json
+from math import ceil
 
 from reports.lib import kshow
 from reports.lib import *
@@ -37,37 +39,69 @@ def tload(ref:Union[RRef,Build])->Task:
     mklens(ref).task_size.val,
     np.load(mklens(ref).ref_koefs.out_ks.syspath))
 
-def teval(x:float,t:Task)->float:
+def teval(x:int,t:Task)->float:
   """ A random function to find the minimum of """
   acc:float=0.0
   for p,k in enumerate(t.ks):
-    acc+=sin(k*x)
+    acc+=sin(k*float(x))
   return acc
 
-def tsuggestX(X:float,t:Task)->float:
+def tsuggestX(X:int,t:Task)->int:
   X2=X+np_choice([1,-1])
   if X2<0:
     X2=float(t.size-1)
   if X2>=t.size:
     X2=0
-  return float(X2)
+  return int(X2)
 
+
+def tsize(t:Task,dim:int)->int:
+  return ceil(t.size**(1.0/dim))
+
+def tflatten(t:Task, X:List[int])->int:
+  S=tsize(t,len(X))
+  k=1
+  acc=0
+  for x in reversed(X):
+    acc+=x*k
+    k*=S
+  return acc
+
+def tsuggestX_dim(X:List[int],t:Task,dim:int)->List[int]:
+  S=tsize(t,dim)
+  while True:
+    X2=np.array(X)+np_choice([1,-1],size=(dim,))
+    x2=tflatten(t,X2)
+    if x2>=0 and x2<t.size:
+      break
+  return X2.tolist()
+
+def teval_dim(xs:List[int],t:Task)->float:
+  """ A random function to find the minimum of """
+  x=tflatten(t,xs)
+  acc:float=0.0
+  for p,k in enumerate(t.ks):
+    acc+=sin(k*float(x))
+  return acc
+
+V=TypeVar('V')
 
 @dataclass_json
 @dataclass
-class MetropolisResults:
-  Xs:List[float]
+class MetropolisResults(Generic[V]):
+  Xs:List[V]
   Ys:List[float]
   Ps:List[float]
   Ac:List[int]
 
 
-def metropolis(F:Callable[[float],float],
-               G:Callable[[float],float],
+
+def metropolis(F:Callable[[V],float],
+               G:Callable[[V],V],
                T:float,
-               X_0:float,
+               X_0:V,
                maxsteps:Optional[int]=100,
-               maxaccepts:Optional[int]=None)->MetropolisResults:
+               maxaccepts:Optional[int]=None)->MetropolisResults[V]:
   """ Find the minimum of `F` using the MCMC algorithm. Evolve the F's argument
   according to the `MC` step function """
   step=0
@@ -128,13 +162,14 @@ task_size=100
            task_size=task_size,
            steps=100*task_size,
            T=4.0,out_results=[selfref,"results.json"],
+           dim=4,
            sourcedeps=[metropolis,teval,tsuggestX])
-def stage_metropolis(name,build,ref_koefs,task_size,steps,T,out_results):
+def stage_metropolis(name,build,ref_koefs,task_size,steps,T,out_results,dim):
   print('Running metropolis')
   t=tload(build)
-  F=partial(teval,t=t)
-  G=partial(tsuggestX,t=t)
-  mr=metropolis(F=F,G=G, T=T,X_0=0,maxsteps=steps)
+  F=partial(teval_dim,t=t)
+  G=partial(tsuggestX_dim,t=t,dim=dim)
+  mr=metropolis(F=F,G=G, T=T,X_0=[0]*dim,maxsteps=steps)
   with open(out_results,'w') as f:
     json_dump(mr.to_dict(),f,indent=4) # type: ignore
 
@@ -145,11 +180,12 @@ def stage_metropolis(name,build,ref_koefs,task_size,steps,T,out_results):
 def stage_findT0(build:Build,ref_metr,T0min,T0max,factor,
                  waitsteps,Paccept_tgt,out_T0):
   t=tload(ref_metr)
-  F=partial(teval,t=t)
-  G=partial(tsuggestX,t=t)
+  dim=mklens(ref_metr).dim.val
+  F=partial(teval_dim,t=t)
+  G=partial(tsuggestX_dim,t=t,dim=dim)
   T0i=float(T0min)
   while T0i<T0max:
-    r=metropolis(F=F,G=G,T=T0i,X_0=0,maxsteps=waitsteps)
+    r=metropolis(F=F,G=G,T=T0i,X_0=[0]*dim,maxsteps=waitsteps)
     Paccept=np.mean(r.Ac)
     print(f"T0i {T0i} Paccept {Paccept}")
     if Paccept>=Paccept_tgt:
@@ -168,12 +204,13 @@ def same(l,rtol)->bool:
 def stage_annealing(build:Build,ref_T0,decay,rtol,patience,out_results,name):
   T=np.load(mklens(build).ref_T0.out_T0.syspath)
   t=tload(mklens(build).ref_T0.ref_metr.rref)
-  F=partial(teval,t=t)
-  G=partial(tsuggestX,t=t)
-  rs:List[MetropolisResults]=[]
+  dim=mklens(build).ref_T0.ref_metr.dim.val
+  F=partial(teval_dim,t=t)
+  G=partial(tsuggestX_dim,t=t,dim=dim)
+  rs:List[MetropolisResults[List[int]]]=[]
   while True:
     print(f"T is {T}")
-    rs.append(metropolis(F=F,G=G,T=T,X_0=rs[-1].Xs[-1] if rs else 0,
+    rs.append(metropolis(F=F,G=G,T=T,X_0=rs[-1].Xs[-1] if rs else [0]*dim,
                          maxaccepts=t.size,
                          maxsteps=10*t.size))
     solutions=[r.Ys[-1] for r in rs[-patience:]]
@@ -204,7 +241,7 @@ def stage_experiment2(r:Registry):
 # |_| \_\\__,_|_| |_|_| |_|\___|_|  |___/
 #
 
-def run()->Tuple[RRef,RRef,MetropolisResults]:
+def run()->Tuple[RRef,RRef,MetropolisResults[int]]:
   (r,rT0),clo,ctx=realize(instantiate(stage_experiment1))
   assert isinstance(r,DRef)
   assert isinstance(rT0,DRef)
@@ -213,23 +250,24 @@ def run()->Tuple[RRef,RRef,MetropolisResults]:
   return (ctx[r][0],ctx[rT0][0],mr)
 
 curref,curT0,curmr=run()
-print('T0 =',np.load(mklens(curT0).out_T0.syspath))
+print(f'T0[dim={mklens(curT0).ref_metr.dim.val}] =',np.load(mklens(curT0).out_T0.syspath))
 
 def runA(force=False)->RRef:
   (_,_,rAnn),clo,ctx=realize(instantiate(stage_experiment2),force_rebuild=force)
   assert isinstance(rAnn,DRef)
   return ctx[rAnn][0]
 
-# currefT0=realize1(instantiate(stage_findT0
 
 def plots(rref=curref,mr=curmr):
   t:Task=tload(rref)
   allXs=np.arange(0,t.size-1)
   Ys=list(map(lambda x:teval(x,t),allXs))
+  Xs1=[teval_dim(x,t) for x in mr.Xs]
+  print('here')
   fig,(axF,axXs,ax1,ax2)=subplots(4)
   axF.plot(allXs,Ys,label='F')
-  axXs.plot(mr.Xs,color='red',label='X(time)')
-  ax1.hist(mr.Xs,label='Visited X')
+  axXs.plot(Xs1,'.',color='red',label='X(time)')
+  ax1.hist(Xs1,label='Visited X')
   ax2.hist(mr.Ys,color='orange',label='Visited Y')
   fig.legend()
   kshow()
@@ -239,14 +277,14 @@ def plots(rref=curref,mr=curmr):
 def plotsA(rref:RRef):
   t:Task=tload(mklens(rref).ref_T0.ref_metr.rref)
   with open(mklens(rref).out_results.syspath) as f:
-    rs=[MetropolisResults.from_dict(x) for x in json_load(f)] # type: ignore
+    rs=[MetropolisResults[List[int]].from_dict(x) for x in json_load(f)] # type: ignore
   fXs=np.arange(0,t.size-1)
   fYs=list(map(lambda x:teval(x,t),fXs))
   fig,(axF,axXs,ax1,ax2)=subplots(4)
   axF.plot(fXs,fYs,label='F')
-  allXs=list(chain(*[r.Xs for r in rs]))
+  allXs=[tflatten(t,x) for x in chain(*[r.Xs for r in rs])]
   allYs=list(chain(*[r.Ys for r in rs]))
-  axXs.plot(allXs,color='red',label='X(time)')
+  axXs.plot(allXs,'.',color='red',label='X(time)')
   ax1.hist(allXs,label='Visited X')
   ax2.hist(allYs,color='orange',label='Visited Y')
   fig.legend()

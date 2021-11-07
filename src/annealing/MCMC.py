1a3d2f27103e1f9c48aec577fcf6352690e2bfeb
Sergey Mironov <grrwlf@gmail.com>
2021-11-07 14:16:28 +0300

    Multiprocessing and other fixes; seems to work fine now

--- src/annealing/MCMC.py
+++ src/annealing/MCMC.py
@@ -26,6 +26,13 @@ from dataclasses_json import dataclass_json
 from reports.lib import kshow
 from reports.lib import *
 
+from multiprocessing import pool,Pool
+def _f(a,f):
+  f(**a)
+def pool_starstarmap(p:pool.Pool, f:Callable[...,Any], lkwargs:List[dict]):
+  p.map(partial(_f,f=f),lkwargs)
+
+
 plt.style.use('dark_background')
 fsinit('_pylightnix',use_as_default=True)
 
@@ -180,7 +187,8 @@ def stage_koef(build:Build,name,index,N,scale,out_ks,task_size):
   np.save(out_ks,r)
 
 
-@autostage(T0min=0.01,
+@autostage(T0min=0.001,
+           Tstart=1,
            T0max=1e6,
            factor=2,
            waitsteps=100,
@@ -191,84 +199,83 @@ def stage_koef(build:Build,name,index,N,scale,out_ks,task_size):
            out_stepsused=[selfref,'stepsused.npy'],
            dim=4,
            sourcedeps=[metropolis,teval,tsuggestX])
-def stage_findT0(build:Build,ref_koef,T0min,T0max,factor,
+def stage_findT0(build:Build,ref_koef,Tstart,T0min,T0max,factor,
                  waitsteps,Paccept_tgt,out_T0,dim,Paccept_sol,out_T1,
                  out_stepsused):
   t=tload(ref_koef._rref)
   F=partial(teval_dim,t=t)
   G=partial(tsuggestX_dim,t=t)
-  X0=np_choice(range(int(t.size**(1/dim))),size=(dim,)).tolist()
-  T0i=float(T0min)
-  T0e=None
+  T0i=Tstart
   nsteps=0
   while T0i<T0max:
+    X0=np_choice(range(int(t.size**(1/dim))),size=(dim,)).tolist()
     r=metropolis(F=F,G=G,T=T0i,X_0=X0,maxsteps=waitsteps)
     Paccept=sum(ac for ac in r.Ac if ac>0)/len(r.Ac)
-    print(f"T0e {T0e} T0i {T0i} Paccept {Paccept}")
+    print(f"T0i {T0i} Paccept {Paccept} steps {nsteps+waitsteps}")
     if Paccept>=Paccept_tgt:
       break
-    if Paccept>=Paccept_sol:
-      if T0e is None:
-        T0e=T0i
-    else:
-      T0e=None
     T0i*=factor
     nsteps+=waitsteps
-  assert T0e is not None
+  T0e=Tstart
+  while T0e>T0min:
+    X0=np_choice(range(int(t.size**(1/dim))),size=(dim,)).tolist()
+    r=metropolis(F=F,G=G,T=T0e,X_0=X0,maxsteps=waitsteps)
+    Paccept=sum(ac for ac in r.Ac if ac>0)/len(r.Ac)
+    print(f"T0e {T0e} Paccept {Paccept} steps {nsteps+waitsteps}")
+    if Paccept<=Paccept_sol:
+      break
+    T0e/=factor
+    nsteps+=waitsteps
   np.save(out_T1,T0e)
   np.save(out_T0,T0i)
   np.save(out_stepsused,nsteps)
-  print(f"Used {nsteps}")
 
 
 def same(l,rtol)->bool:
   return all(np.isclose(x,l[-1],rtol=rtol) for x in l)
 
 
-def _worker(a)->None:
-  def _w(ref_T0,decay,rtol,patience,out_results,name,
-         maxsteps,maxaccepts,rindex,out_rindex):
-    numpy.random.seed(13*rindex)
-    T=float(np.load(ref_T0.out_T0))
-    assert T>0.0, f"T0={T}<=0.0 ??"
-    T1=np.load(ref_T0.out_T1)
-    assert T1>0.0, f"T1={T1}<=0.0 ??"
-    t=tload(ref_T0.ref_koef._rref)
-    maxaccepts2=t.size if maxaccepts is None else maxaccepts
-    task_size=ref_T0.ref_koef.task_size
-    budget=task_size-np.load(ref_T0.out_stepsused)
-    assert budget>0, f"{budget}<=0 ??"
-    dim=ref_T0.dim
-    X0=np_choice(range(int(task_size**(1/dim))),size=(dim,)).tolist()
-    F=partial(teval_dim,t=t)
-    G=partial(tsuggestX_dim,t=t)
-    rs:List[MetropolisResults[List[int]]]=[]
-    while True:
-      maxloops=log(T1/T,decay)
-      if budget<=0 or maxloops<=0:
-        break
-      maxsteps=min(1000,task_size/20,int(budget/maxloops)) # Maxsteps per T
-      assert maxsteps>0, f"{maxsteps}<=0 ??"
-      rs.append(metropolis(F=F,G=G,T=T,X_0=rs[-1].Xs[-1] if rs else X0,
-                           maxaccepts=maxaccepts2,
-                           maxsteps=maxsteps))
-      nsteps=sum(len(r.Xs) for r in rs)
-      solutions=[r.Ys[-1] for r in rs[-patience:]]
-      print(f"T {T:0.3f} ns {nsteps} bu {budget} ms {maxsteps} : {solutions}")
-      if len(solutions)==patience and same(solutions,rtol):
-        break
-      with open(out_results,'w') as f:
-        acc=[]
-        for r in rs:
-          dd=r.to_dict() # type: ignore
-          acc.append(dd)
-        json_dump(acc,f,indent=4)
-      T*=decay
-      budget-=len(rs[-1].Xs)
-      np.save(out_rindex,rindex)
-  return _w(**a.__dict__)
-
-from multiprocessing import Pool
+def _worker(ref_T0,decay,rtol,patience,out_results,name,
+         maxsteps,maxaccepts,rindex,out_rindex)->None:
+  print(ref_T0)
+  numpy.random.seed(13*rindex)
+  T=float(np.load(ref_T0.out_T0))
+  assert T>0.0, f"T0={T}<=0.0 ??"
+  T1=np.load(ref_T0.out_T1)
+  assert T1>0.0, f"T1={T1}<=0.0 ??"
+  t=tload(ref_T0.ref_koef._rref)
+  maxaccepts2=t.size if maxaccepts is None else maxaccepts
+  task_size=ref_T0.ref_koef.task_size
+  budget=task_size-np.load(ref_T0.out_stepsused)
+  assert budget>0, f"{budget}<=0 ??"
+  dim=ref_T0.dim
+  X0=np_choice(range(int(task_size**(1/dim))),size=(dim,)).tolist()
+  F=partial(teval_dim,t=t)
+  G=partial(tsuggestX_dim,t=t)
+  rs:List[MetropolisResults[List[int]]]=[]
+  while True:
+    maxloops=log(T1/T,decay)
+    if budget<=0 or maxloops<=0:
+      break
+    maxsteps=min(1000,task_size/20,int(budget/maxloops)) # Maxsteps per T
+    assert maxsteps>0, f"{maxsteps}<=0 ??"
+    rs.append(metropolis(F=F,G=G,T=T,X_0=rs[-1].Xs[-1] if rs else X0,
+                         maxaccepts=maxaccepts2,
+                         maxsteps=maxsteps))
+    nsteps=sum(len(r.Xs) for r in rs)
+    solutions=[r.Ys[-1] for r in rs[-patience:]]
+    print(f"T {T:0.3f} ns {nsteps} bu {budget} ms {maxsteps} : {solutions}")
+    if len(solutions)==patience and same(solutions,rtol):
+      break
+    with open(out_results,'w') as f:
+      acc=[]
+      for r in rs:
+        dd=r.to_dict() # type: ignore
+        acc.append(dd)
+      json_dump(acc,f,indent=4)
+    T*=decay
+    budget-=len(rs[-1].Xs)
+    np.save(out_rindex,rindex)
 
 @autostage_(decay=0.85,
             rtol=0.01,
@@ -282,7 +289,7 @@ from multiprocessing import Pool
             nouts=10)
 def stage_annealing(build:Build,arglist):
   with Pool() as p:
-    p.map(_worker,arglist)
+    pool_starstarmap(p, _worker, [a.__dict__ for a in arglist])
 
 @dataclass_json
 @dataclass
@@ -366,7 +373,7 @@ def runA(force=False,dim=10,task_size=TASK_SIZE,view=0)->None:
 def run2(force=False,task_size=TASK_SIZE)->None:
   acc:list=[]
   bf_mean:list=[]; bf_std:list=[]; ann_budgets:list=[]
-  dims=list(range(15))[3:]
+  dims=list(range(10))[1:]
   for dim in dims:
     print(f"dim {dim}")
     r=Registry()

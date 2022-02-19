6f051d9488b882025ff0d88b3be6a182226422b6
Sergey Mironov <grrwlf@gmail.com>
2022-02-19 16:20:50 +0300

    Update env

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 export PROJECT_SOURCE=`pwd`
-export PYTHONPATH=`pwd`/src:`pwd`/modules/sinara-config-builder/src:`pwd`/modules/qiskit-terra:`pwd`/modules/qboard-sdk:`pwd`/modules/pylightnix/src:$PYTHONPATH
-export MYPYPATH=`pwd`/src:`pwd`/modules/sinara-config-builder/src:`pwd`/modules/qboard-sdk:`pwd`/modules/pylightnix/src:`pwd`/modules/qiskit-terra:`pwd`/modules/qiskit-aer
+export PYTHONPATH=`pwd`/src:`pwd`/modules/pylightnix/src:$PYTHONPATH
+export MYPYPATH=`pwd`/src:`pwd`/modules/pylightnix/src
 export PATH=`pwd`/scripts:$PATH
 alias ipython="sh `pwd`/ipython.sh"
 export PYLIGHTNIX_ROOT=`pwd`/_pylightnix
